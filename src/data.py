b4cc87f1c46f6c31df178c83d86072183025f93e
nguy-roland <nguyroland.zongo@proton.me>
2026-07-22 16:15:36 +0000

    feat: switch input resolution to 224x224 for ResNet compatibility

--- src/data.py
+++ src/data.py
@@ -10,7 +10,7 @@ MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
 train_transform = transforms.Compose([
-    transforms.Resize((64, 64)),
+    transforms.Resize((224, 224)),
     transforms.RandomRotation(15),
     transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2),
     transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),
@@ -19,7 +19,7 @@ train_transform = transforms.Compose([
 ])
 
 val_transform = transforms.Compose([
-    transforms.Resize((64, 64)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(MEAN, STD),
 ])
